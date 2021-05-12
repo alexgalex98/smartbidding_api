bd8ab531c8555cf536fd7aef464506a1a8759f27
alexgalex98 <73701449+alexgalex98@users.noreply.github.com>
2021-05-12 15:47:47 +0300

    working sign in, register

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const cors=require('cors')
+const bodyParser=require('body-parser')
+const signin=require('./controllers/signin')
+const bcrypt=require('bcrypt-nodejs')
+const register=require('./controllers/register')
 
 const app=express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const knex = require('knex')({
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'postgre',
+      database : 'smart-brain'
+    }
+  });
+
+console.log(knex.select().from('users').then(data=>{
+    // console.log(data)
+}))
+
 
 app.get('/', (req,res)=>{
     res.send("this is working")
 })
 
-app.listen(30030,()=>{
+app.post('/signin',(req,res)=>{
+    signin.handleSignIn(req,res,knex,bcrypt)
+})
+
+app.post('/register',(req,res)=>{
+    register.handleRegister(req,res,knex,bcrypt)
+})
+
+app.listen(3000,()=>{
     console.log("app is running on port 3000")
 })
